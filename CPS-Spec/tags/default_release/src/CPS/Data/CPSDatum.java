1d2e2a142eaec576f0777941fdcb999e280c9b41
(no author) <(no author)@faae0253-b743-0410-ada9-4f8a1c16cf08>
2007-03-15 16:17:47 +0000

    This commit was manufactured by cvs2svn to create tag 'default_release'.
    git-svn-id: https://cropplanning.googlecode.com/svn@27 faae0253-b743-0410-ada9-4f8a1c16cf08

---
+++ CPS-Spec/tags/default_release/src/CPS/Data/CPSDatum.java
@@ -0,0 +1,17 @@
+
+package CPS.Data;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+
+public abstract class CPSDatum {
+
+    abstract JLabel toLabel();
+    abstract JComponent toEditableField();
+    abstract JComponent toStaticField();
+
+//    abstract String toTableHeader();
+//    abstract void getTableFormat();
+//    abstract void getTableData();
+
+}
---
+++ CPS-Spec/tags/default_release/src/CPS/Data/PlantingDatum.java
@@ -0,0 +1,31 @@
+
+package CPS.Data;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+
+public class PlantingDatum extends CPSDatum {
+   JLabel toLabel() {
+      
+      return null;
+   }
+
+   JComponent toEditableField() {
+      return null;
+   }
+
+   JComponent toStaticField() {
+      return null;
+   }
+
+   String toTableHeader() {
+      return null;
+   }
+
+   void getTableFormat() {
+   }
+
+   void getTableData() {
+   }
+
+}
