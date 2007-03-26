a78cc8888199bc67fb0f65416eabb53066dd21c1
Clayton <Clayton@faae0253-b743-0410-ada9-4f8a1c16cf08>
2007-03-26 14:18:16 +0000

    Seperated frame and window code from more general UI code; installed basics for module update refresh.
    git-svn-id: https://cropplanning.googlecode.com/svn@35 faae0253-b743-0410-ada9-4f8a1c16cf08

---
+++ CPS-SwingUI/trunk/src/CPS/Core/UI/FrameManger.java
@@ -0,0 +1,84 @@
+/*
+ * FrameManger.java
+ *
+ * Created on March 18, 2007, 1:34 PM by Clayton
+ */
+
+package CPS.Core.UI;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class FrameManger {
+   
+   private JFrame frame;
+   private JPanel contentPane;
+   
+   public FrameManger() {
+      
+      frame = createFrame( GraphicsEnvironment.getLocalGraphicsEnvironment().
+                           getDefaultScreenDevice().getDefaultConfiguration() );
+      
+   }
+ 
+   
+    /**
+     * Create a frame for SwingSet2 to reside in if brought up
+     * as an application.
+     */
+    public static JFrame createFrame(GraphicsConfiguration gc) {
+	JFrame frame = new JFrame(gc);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	WindowListener l = new WindowAdapter() {
+	        public void windowClosing(WindowEvent e) {
+	        }
+	    };
+	frame.addWindowListener(l);
+	return frame;
+    }
+
+    public void buildFrame() {
+	if( getFrame() != null ) {
+	    // put swingset in a frame and show it
+	    JFrame f = getFrame();
+	    //f.getContentPane().add(f, BorderLayout.CENTER);
+
+	    // Must use a top level container for contentPane,
+	    // not wise to use something like tabbedPane
+	    contentPane = new JPanel( new BorderLayout() );
+	    f.setContentPane( contentPane );
+
+	    
+	    Rectangle screenRect = f.getGraphicsConfiguration().getBounds();
+            Insets screenInsets = Toolkit.getDefaultToolkit().getScreenInsets(
+                    f.getGraphicsConfiguration());
+
+            // Make sure we don't place the demo off the screen.
+            int centerWidth = screenRect.width < f.getSize().width ?
+                    screenRect.x :
+                    screenRect.x + screenRect.width/2 -	f.getSize().width/2;
+            int centerHeight = screenRect.height < f.getSize().height ?
+                    screenRect.y :
+                    screenRect.y + screenRect.height/2 - f.getSize().height/2;
+
+            centerHeight = centerHeight < screenInsets.top ?
+                    screenInsets.top : centerHeight;
+
+            f.setLocation(centerWidth, centerHeight);
+	} 
+    }
+
+    void showFrame() {
+       getFrame().setVisible(true);
+    }
+
+    /**
+     * Returns the frame instance
+     */
+    public JFrame getFrame() {
+	return frame;
+    }
+
+}
--- CPS-SwingUI/trunk/src/CPS/Core/UI/TabbedUI.java
+++ CPS-SwingUI/trunk/src/CPS/Core/UI/TabbedUI.java
@@ -10,10 +10,7 @@ import javax.swing.colorchooser.*;
 import javax.swing.filechooser.*;
 import javax.accessibility.*;
 
-import javax.swing.plaf.metal.DefaultMetalTheme;
-import javax.swing.plaf.metal.MetalLookAndFeel;
 
-import java.lang.reflect.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.beans.*;
@@ -29,60 +26,34 @@ public class TabbedUI extends CPSUIModule {
     String ModuleType = "UI";
     String ModuleVersion = ".1";
 
-    private static final String windows  =
-            "com.sun.java.swing.plaf.windows.WindowsLookAndFeel";
-
-    // The current Look & Feel
-    private static String currentLookAndFeel = windows;
 
     // The preferred size of the demo
     private static final int PREFERRED_WIDTH = 720;
     private static final int PREFERRED_HEIGHT = 640;
     
-    // Box spacers
-    private Dimension HGAP = new Dimension(1,5);
-    private Dimension VGAP = new Dimension(5,1);
 
     // A place to hold on to the visible demo
     // private JPanel contentPane = null;
     JTabbedPane tabbedpane;
-
-    // Used only if swingset is an application 
-    private JFrame frame = null;
-    private JWindow splashScreen = null;
+    FrameManger fm;
+    
+    private ArrayList<ModuleListElement> moduleList;
+    private boolean modulesUpdated;
 
     // To debug or not to debug, that is the question
     private boolean DEBUG = true;
     private int debugCounter = 0;
 
-    // contentPane cache, saved from the applet or application frame
-    Container contentPane = null;
 
     /**
      * SwingSet2 Constructors
      */
     public TabbedUI() {	
        
-       frame = createFrame( GraphicsEnvironment.
-	                    getLocalGraphicsEnvironment().
-			     getDefaultScreenDevice().
-			     getDefaultConfiguration() );
-	// showTabbedUI();
-    }
-
-    /**
-     * Create a frame for SwingSet2 to reside in if brought up
-     * as an application.
-     */
-    public static JFrame createFrame(GraphicsConfiguration gc) {
-	JFrame frame = new JFrame(gc);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	WindowListener l = new WindowAdapter() {
-	        public void windowClosing(WindowEvent e) {
-	        }
-	    };
-	frame.addWindowListener(l);
-	return frame;
+       fm = new FrameManger();
+       moduleList = new ArrayList<ModuleListElement>();
+       setModulesUpdated( false );
+       
     }
 
     /**
@@ -90,43 +61,19 @@ public class TabbedUI extends CPSUIModule {
      * applicable if coming up as an application, not an applet);
      */
     public void showUI() {
-	if(getFrame() != null) {
-	    // put swingset in a frame and show it
-	    JFrame f = getFrame();
-	    f.setTitle(getString( ModuleName ));
-	    //f.getContentPane().add(f, BorderLayout.CENTER);
+       
+       fm.getFrame().setTitle( ModuleName );
+       
+       // initialize tabs
+       tabbedpane = new JTabbedPane();
+       tabbedpane.setPreferredSize(new Dimension( PREFERRED_WIDTH,
+                                                  PREFERRED_HEIGHT ));
+       fm.getFrame().getContentPane().add( tabbedpane, BorderLayout.CENTER );
 
-	    // Must use a top level container for contentPane,
-	    // not wise to use something like tabbedPane
-	    contentPane = new JPanel( new BorderLayout() );
-	    f.setContentPane( contentPane );
-
-	    // initialize tabs
-	    tabbedpane = new JTabbedPane();
-	    tabbedpane.setPreferredSize(new Dimension( PREFERRED_WIDTH,
-						       PREFERRED_HEIGHT ));
-	    f.getContentPane().add(tabbedpane, BorderLayout.CENTER);
-
-	    finishAddingTab( f );
-	    
-	    Rectangle screenRect = f.getGraphicsConfiguration().getBounds();
-            Insets screenInsets = Toolkit.getDefaultToolkit().getScreenInsets(
-                    f.getGraphicsConfiguration());
-
-            // Make sure we don't place the demo off the screen.
-            int centerWidth = screenRect.width < f.getSize().width ?
-                    screenRect.x :
-                    screenRect.x + screenRect.width/2 -	f.getSize().width/2;
-            int centerHeight = screenRect.height < f.getSize().height ?
-                    screenRect.y :
-                    screenRect.y + screenRect.height/2 - f.getSize().height/2;
-
-            centerHeight = centerHeight < screenInsets.top ?
-                    screenInsets.top : centerHeight;
-
-            f.setLocation(centerWidth, centerHeight);
-	    f.setVisible( true );
-	} 
+       addModules();
+       
+       fm.getFrame().setVisible( true );
+ 
     }
 
     /**
@@ -136,13 +83,34 @@ public class TabbedUI extends CPSUIModule {
 
 	content.setBorder( BorderFactory.createTitledBorder( name + 
 							     " border" ));
-	tabbedpane.addTab( name, content );
-
-	finishAddingTab( getFrame() );
+        
+        moduleList.add( new ModuleListElement( name, content ));
+        setModulesUpdated( true );
+        
+        addModules();
 
     }
 
-    private void finishAddingTab( JFrame f ) {
+    private void addModules() {
+       if ( tabbedpane == null || ! areModulesUpdated() )
+          return;
+       
+       Iterator i = moduleList.iterator();
+       ModuleListElement mle;
+          
+       tabbedpane.removeAll();
+          
+       while ( i.hasNext() ) {
+          mle = (ModuleListElement) i.next();
+          tabbedpane.addTab( mle.getName(), mle.getContent() );
+       }
+
+       finishAddingTabs();
+       setModulesUpdated( false );
+       
+    }
+    
+    private void finishAddingTabs() {
 
 	tabbedpane.getModel().addChangeListener(
 	  new ChangeListener() {
@@ -157,21 +125,7 @@ public class TabbedUI extends CPSUIModule {
 	  );
 
 
-	f.pack();
-    }
-
-    /**
-     * Returns the frame instance
-     */
-    public JFrame getFrame() {
-	return frame;
-    }
-
-    /**
-     * This method returns a string from the demo's resource bundle.
-     */
-    public String getString(String key) {
-	return key;
+	fm.getFrame().pack();
     }
 
     /**
@@ -189,5 +143,28 @@ public class TabbedUI extends CPSUIModule {
 	return ModuleVersion;
     }
 
+    
+    private class ModuleListElement {
+       private String name;
+       private JPanel content;
+
+       public ModuleListElement( String n, JPanel c ) {
+          setName(n);
+          setContent(c);
+       }
+      public String getName() { return name; }
+      public void setName(String name) { this.name = name; }
+      public JPanel getContent() { return content; }
+      public void setContent(JPanel content) { this.content = content; }
+       
+    }
+
+   public boolean areModulesUpdated() {
+      return modulesUpdated;
+   }
+
+   public void setModulesUpdated( boolean modulesUpdated ) {
+      this.modulesUpdated = modulesUpdated;
+   }
 }
 
