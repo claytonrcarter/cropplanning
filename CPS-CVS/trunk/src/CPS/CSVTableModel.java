44b92534d3f621835656c267be9e20e4fc159fff
Clayton <Clayton@faae0253-b743-0410-ada9-4f8a1c16cf08>
2007-04-05 16:36:40 +0000

    Better CSV parsing w/ JavaCSV
    git-svn-id: https://cropplanning.googlecode.com/svn@50 faae0253-b743-0410-ada9-4f8a1c16cf08

--- CPS-CVS/trunk/src/CPS/CSVTableModel.java
+++ CPS-CVS/trunk/src/CPS/CSVTableModel.java
@@ -6,20 +6,13 @@
 
 package CPS;
 
-/* From: http://forum.java.sun.com/thread.jspa?threadID=5138036&messageID=9506301
- */
-
-import java.io.File;
-import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import javax.swing.table.AbstractTableModel;
 import com.csvreader.CsvReader;
 
 class CSVTableModel extends AbstractTableModel {
-    RandomAccessFile raf;
-    // this keeps a file position (char number) to indicate each line
-    ArrayList<Long> lineToPos = new ArrayList<Long>();
+   
+    ArrayList<String[]> lines = new ArrayList<String[]>();
     int columnCount = -1;
     String[] colNames = null;
     
@@ -30,6 +23,8 @@ class CSVTableModel extends AbstractTableModel {
        try {
            
           csv = new CsvReader( fileName );
+          csv.setDelimiter(',');
+          csv.setTextQualifier('"');
        
           if ( csv.readHeaders() ) {
              colNames = csv.getHeaders();
@@ -37,55 +32,29 @@ class CSVTableModel extends AbstractTableModel {
                 columnCount = colNames.length;
           }
           
-          // raf = new RandomAccessFile( new File(fileName), "r" );
-            
-          // commented so that we do not count the header line
-          // lineToPos.add(new Long(0));
-//          String line = raf.readLine();
-//          if ( line != null ) {
-//             colNames = line.split(",");
-//             if ( columnCount == -1 )
-//                columnCount = colNames.length;
-//             lineToPos.add( new Long( raf.getFilePointer() ));
-//          // }
-            
-            // figure out the number of lines in the file
-            while ((line = raf.readLine()) != null) {
-                lineToPos.add( new Long( raf.getFilePointer() ));
-            }
-            lineToPos.remove(lineToPos.size()-1);
+          while ( csv.readRecord() )
+             lines.add( csv.getValues() );
+          
         } catch ( Exception e ) { e.printStackTrace(); }
     }
     
-    protected void finalize() throws Throwable {
-        super.finalize();
-        raf.close();
-    }
- 
     public int getColumnCount() {
         return columnCount;
     }
  
     public int getRowCount() {
-        return lineToPos.size();
+        return lines.size();
     }
  
     public Object getValueAt(int rowIndex, int columnIndex) {
        
        if ( columnIndex >= columnCount )
           System.err.println("Column out of bounds: " + columnIndex + " greater than " + columnCount );
-       if ( rowIndex > lineToPos.size() - 1 )
-          System.err.println("Row out of bounds: " + rowIndex + " greater than " + ( lineToPos.size() - 1 ));
+       if ( rowIndex > lines.size() - 1 )
+          System.err.println("Row out of bounds: " + rowIndex + " greater than " + ( lines.size() - 1 ));
        
-       try {
-          raf.seek(lineToPos.get(rowIndex).longValue());
-          String line = raf.readLine();
-          String[] strs = line.split( ",", columnCount ); // could also use -1 instead of columnCount
-          return strs[columnIndex];
-       } catch (IOException e) {
-          e.printStackTrace();
-       }
-       return null;
+       return lines.get(rowIndex)[columnIndex];
+    
     }
     
     public String getStringAt( int r, int c ) {
