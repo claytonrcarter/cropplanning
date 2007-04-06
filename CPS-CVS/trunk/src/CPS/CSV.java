b3e5572ee06afe992e1f66be569ae90ead788935
Clayton <Clayton@faae0253-b743-0410-ada9-4f8a1c16cf08>
2007-04-06 17:05:10 +0000

    improved export behavior
    git-svn-id: https://cropplanning.googlecode.com/svn@57 faae0253-b743-0410-ada9-4f8a1c16cf08

--- CPS-CVS/trunk/src/CPS/CSV.java
+++ CPS-CVS/trunk/src/CPS/CSV.java
@@ -23,9 +23,10 @@ public class CSV extends CPSDataModel {
    public ArrayList<CPSCrop> exportCropsAndVarieties() {
       ArrayList<CPSCrop> l = new ArrayList<CPSCrop>();
       CPSCrop temp;
-      
+       
+      System.out.println( "Exporting data." );
+        
       for ( int i = 0; i < ctm.getRowCount(); i++ ) {
-         System.out.println("Export data for crop: " + ctm.getStringAt( i, 0 ));
          l.add( this.getCropInfoForRow(i) );
       }
       
@@ -34,56 +35,52 @@ public class CSV extends CPSDataModel {
 
    
    public CPSCrop getCropInfoForRow( int selectedRow ) {
+        
+      CPSCrop temp = new CPSCrop();
+         
+      //"crop_name","var_name","similar_to","bot_name","fam_name",
+      //"Groups","Successions","Desc","Keywords","Fudge","other_req","Notes",
+      //"Maturity","mat_adjust","misc_adjust",
+      //"seeds_sources","seeds_item_codes","seeds_unit_size"
+         
+      //temp.setID( (int) ( Math.random() * 10000 ) );
+      temp.setID( selectedRow );
+      int col = 0;
+      temp.setCropName( ctm.getStringAt( selectedRow, col++ ) );
+      temp.setVarietyName( ctm.getStringAt( selectedRow, col++ ));
+
+      temp.setSimilarCrop( this.getCropInfo( ctm.getStringAt( selectedRow, col++ ) ));
       
-         CPSCrop temp = new CPSCrop();
+      temp.setBotanicalName( ctm.getStringAt( selectedRow, col++ ));
+      temp.setFamilyName( ctm.getStringAt( selectedRow, col++ ));
+
+      temp.setGroups( ctm.getStringAt( selectedRow, col++ ));
+      temp.setSuccessions( Boolean.parseBoolean( ctm.getStringAt( selectedRow, col++ )));
+      temp.setCropDescription( ctm.getStringAt( selectedRow, col++ ));
+      temp.setKeywords( ctm.getStringAt( selectedRow, col++ ));
+      col++; // fudge
+      temp.setOtherRequirements( ctm.getStringAt( selectedRow, col++ ));
+      temp.setNotes( ctm.getStringAt( selectedRow, col++ ));
          
-         //"crop_name","var_name","similar_to","bot_name","fam_name",
-         //"Groups","Successions","Desc","Keywords","Fudge","other_req","Notes",
-         //"Maturity","mat_adjust","misc_adjust",
-         //"seeds_sources","seeds_item_codes","seeds_unit_size"
-         
-         //temp.setID( (int) ( Math.random() * 10000 ) );
-         temp.setID( selectedRow );
-         int col = 0;
-         temp.setCropName( ctm.getStringAt( selectedRow, col++ ) );
-         temp.setVarietyName( ctm.getStringAt( selectedRow, col++ ));
-         temp.setSimilarCrop( this.getCropInfo( ctm.getStringAt( selectedRow, col++ ) ));
-         temp.setBotanicalName( ctm.getStringAt( selectedRow, col++ ));
-         temp.setFamilyName( ctm.getStringAt( selectedRow, col++ ));
-         
-         temp.setGroups( ctm.getStringAt( selectedRow, col++ ));
-         temp.setSuccessions( Boolean.parseBoolean( ctm.getStringAt( selectedRow, col++ )));
-         temp.setCropDescription( ctm.getStringAt( selectedRow, col++ ));
-         temp.setKeywords( ctm.getStringAt( selectedRow, col++ ));
-         col++; // fudge
-         temp.setOtherRequirements( ctm.getStringAt( selectedRow, col++ ));
-         temp.setNotes( ctm.getStringAt( selectedRow, col++ ));
-         
-         temp.setMaturityDays( ctm.getIntAt( selectedRow, col++ ));
+      temp.setMaturityDays( ctm.getIntAt( selectedRow, col++ ));
       
-         return temp;
-   
+      return temp;
+
    }
    
    public CPSCrop getCropInfo( String cropName ) {
    
-      if ( cropName.equals("") )
-         return null;
+      CPSCrop temp = new CPSCrop();
       
-      /*
-       * String[] s = cropName.split( "::" );
-       * then s[0] == cropname and s[1] == varname
-       */
-      CPSCrop temp = null;
-      
-      for ( int i = 0; i < ctm.getRowCount(); i ++ ) {
-         // match crop_name ==  cropName and var_name == ""
-         if ( ctm.getStringAt( i, 0 ).equalsIgnoreCase( cropName ) && 
-              ctm.getStringAt( i, 1 ).equalsIgnoreCase( "" ) ) { 
-            temp = getCropInfoForRow( i );
-            break;
+      if ( ! cropName.equals("") )
+         for ( int i = 0; i < ctm.getRowCount(); i ++ ) {
+            // match crop_name == cropName and var_name == ""
+            if ( ctm.getStringAt( i, 0 ).equalsIgnoreCase( cropName ) &&
+                 ctm.getStringAt( i, 1 ).equalsIgnoreCase( "" ) ) {
+               temp = getCropInfoForRow( i );
+               break;
+            }
          }
-      }
       
       return temp;
    }
@@ -102,5 +99,12 @@ public class CSV extends CPSDataModel {
    
    public void updateCrop(CPSCrop crop) {}
    public void createCrop(CPSCrop crop) {}
+
+   public TableModel getCropList(String sortCol) { return null; }
+   public TableModel getAbbreviatedCropList(String sortCol) { return null; }
+   public TableModel getVarietyList(String sortCol) { return null; }
+   public TableModel getAbbreviatedVarietyList(String sortCol) { return null; }
+   public TableModel getCropAndVarietyList(String sortCol) { return null; }
+   public TableModel getAbbreviatedCropAndVarietyList(String sortCol) { return null; }
    
 }
