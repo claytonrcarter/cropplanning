5668f51d2959dbf7d4b909a2d248e91421e5401e
cropplanning <cropplanning@faae0253-b743-0410-ada9-4f8a1c16cf08>
2008-04-22 11:27:24 +0000

    Includes import code from sm_zl_kimi Refactored package name Refactored main class out of CPSDataModel since it never really was and since we now have a better import/export infrastructure.
    git-svn-id: https://cropplanning.googlecode.com/svn@327 faae0253-b743-0410-ada9-4f8a1c16cf08

--- CPS-CVS/trunk/src/CPS/CSV/CSVTableModel.java
+++ CPS-CVS/trunk/src/CPS/CSV/CSVTableModel.java
@@ -20,7 +20,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package CPS;
+package CPS.CSV;
 
 import java.util.ArrayList;
 import javax.swing.table.AbstractTableModel;
